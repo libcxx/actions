24e3803e3f31df3496dcd23dae47e77e011a2304
Eric Fiselier <eric@efcs.ca>
2020-04-01 01:10:22 -0400

    dummy commit

--- publish/index.js
+++ publish/index.js
@@ -1,41 +1,13 @@
 const core = require('@actions/core');
-const io = require('@actions/io');
-const artifact = require('@actions/artifact');
-const glob = require('@actions/glob');
-const path = require('path');
-const fs = require('fs');
-const {
-  getRuntimeList,
-  checkoutRuntimes,
-  configureRuntimes,
-  buildRuntimes,
-  createActionPaths,
-  installRuntimes,
-  getActionConfig,
-  testRuntime
-} = require('../src/setup-action');
-const {createTestSuiteAnnotations} = require('../src/lit_utils');
-const xunitViewer = require('xunit-viewer');
+const {createAndPublishTestSuiteResults} = require('../src/publish-action');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const action_paths = getActionConfig();
-    
-
-    return;
-/*
-    await xunitViewer({
-      server: false,
-      results: input,
-      ignore: ['_thingy', 'invalid'],
-      title: 'Xunit View Sample Tests',
-      output: 'output.html'
-    });
-    const files = ['output.html'];
-    const uploadResponse = await artifactClient.uploadArtifact(artifactName,
-        files, rootDirectory, artifactOptions);
-    */
+    const token = core.getInput('publisher_key');
+    const config_name = core.getInput('config_name');
+    await createAndPublishTestSuiteResults(action_paths, config_name, token);
   } catch (error) {
     core.setFailed(error.message);
   }
