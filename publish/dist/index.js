b8e281ed918c9d2a0a6e5395950c71b738409f2a
Eric Fiselier <eric@efcs.ca>
2020-04-01 12:56:44 -0400

    dummy commit

--- publish/dist/index.js
+++ publish/dist/index.js
@@ -25292,105 +25292,123 @@ const utils = __webpack_require__(1);
 const xunitViewer = __webpack_require__(721);
 const temp = __webpack_require__(559).track();
 
-
+async function withSSHKey(token, then) {
+  let tempFile = await utils.createTempFile('id_rsa', token);
+  process.env['GIT_SSH_COMMAND'] = `ssh -i ${tempFile}`;
+  var result;
+  try {
+    result = await then();
+  } finally {
+    await utils.unlinkIgnoreError(tempFile);
+    delete process.env['GIT_SSH_COMMAND'];
+  }
+  return result;
+}
 
 async function checkoutLibcxxIO(out_path, branch = 'master') {
   let result = await core.group('checkout', async () => {
     const agent = 'publisher'
     const repo_url = `git@github.com:libcxx/libcxx.github.io.git`;
-    let l = await utils.run('git', ['clone', '--depth=1', '-b', branch, repo_url, out_path]);
-    const opts = {cwd: out_path};
-    await utils.run('git', ['config', '--local', 'user.name', `libc++ Actions ${agent}`], opts);
-    await utils.run('git', ['config', '--local', 'user.email', 'agent@efcs.ca'], opts);
+
+    let l = await utils.run(
+        'git', [ 'clone', '--depth=1', '-b', branch, repo_url, out_path ]);
+    const opts = {cwd : out_path};
+    await utils.run(
+        'git', [ 'config', '--local', 'user.name', `libc++ Actions ${agent}` ],
+        opts);
+    await utils.run(
+        'git', [ 'config', '--local', 'user.email', 'agent@efcs.ca' ], opts);
     return l;
   });
   return result;
 }
 
-
 async function commitChanges(repo_path, destination_name) {
-  const opts = {cwd: repo_path};
-  await utils.run('git', ['add', '-A', ':/'], opts);
-  await utils.run('git', ['commit', '-am', `Publish testsuite results for ${destination_name}`], opts);
+  const opts = {cwd : repo_path};
+  await utils.run('git', [ 'add', '-A', ':/' ], opts);
+  await utils.run(
+      'git',
+      [ 'commit', '-am', `Publish testsuite results for ${destination_name}` ],
+      opts);
 }
 
-
-async function commitAndPushChanges(repo_path, destination_name, token) {
+async function commitAndPushChanges(repo_path, destination_name) {
   await commitChanges(repo_path, destination_name);
-  let tempFile = await utils.createTempFile('id_rsa', token);
-  var result;
-  try {
-    let env = process.env;
-    env['GIT_SSH_COMMAND'] = `ssh -i ${tempFile}`;
-    result = await utils.run(`git -C ${repo_path} push`, [], {env});
-  } finally {
-    await temp.cleanupSync();
-  }
+  let result = await utils.run(`git -C ${repo_path} push`);
   return result;
 }
 
 async function publishTestSuiteHTMLResults(results_file, destination, token) {
   repo_path = 'libcxx.github.io';
-  try {
-    core.saveState('libcxx-io', repo_path);
-    await checkoutLibcxxIO(repo_path);
+  await withSSHKey(token, async () => {
+    try {
+      core.saveState('libcxx-io', repo_path);
+      await checkoutLibcxxIO(repo_path);
 
-    const timestamp = new Date().toISOString();
-    output_path = path.join(repo_path, 'results', destination);
+      const timestamp = new Date().toISOString();
+      output_path = path.join(repo_path, 'results', destination);
 
-    if (!fs.existsSync(output_path)) {
-      await utils.mkdirP(output_path);
+      if (!fs.existsSync(output_path)) {
+        await utils.mkdirP(output_path);
+      }
+
+      const output_file = `results-${timestamp}.html`;
+      await io.cp(results_file, path.join(output_path, output_file));
+
+      index = path.join(output_path, 'index.html');
+      if (fs.existsSync(index)) {
+        await utils.unlink(index);
+      }
+      await fs.symlinkSync(index, `./${output_file}`);
+
+      await commitAndPushChanges(repo_path, destination);
+    } finally {
+      await utils.rmRfIgnoreError(repo_path);
     }
-
-    const output_file = `results-${timestamp}.html`;
-    await io.cp(results_file, path.join(output_path, output_file));
-
-    index = path.join(output_path, 'index.html');
-    if (fs.existsSync(index)) {
-      await utils.unlink(index);
-    }
-    await fs.symlinkSync(index, `./${output_file}`);
-
-    await commitAndPushChanges(repo_path, destination, token);
-  } finally {
-    utils.rmRfIgnoreError(repo_path);
-  }
+  });
 }
 
-async function createTestSuiteHTMLResults(title, xml_file_path, html_output_path) {
-  await Promise.all([xunitViewer({
-      server: false,
-      results: xml_file_path,
-      title: title,
-      output: html_output_path
-    })]
-    );
+async function createTestSuiteHTMLResults(title, xml_file_path,
+                                          html_output_path) {
+  await Promise.all([ xunitViewer({
+    server : false,
+    results : xml_file_path,
+    title : title,
+    output : html_output_path
+  }) ]);
   return 0;
 }
 
 async function publishArtifacts(artifacts_dir) {
-  let p = await core.group("upload-artifacts",  async () => {
+  let p = await core.group("upload-artifacts", async () => {
     const artifactClient = artifact.create();
     let files = await utils.globDirectoryRecursive(artifacts_dir);
-    return artifactClient.uploadArtifact(
-          `test-suite-results`, files,
-          artifacts_dir);
+    return artifactClient.uploadArtifact(`test-suite-results`, files,
+                                         artifacts_dir);
   });
   return p;
 }
 
-
-async function createAndPublishTestSuiteResults(action_paths, config_name, token) {
+async function createAndPublishTestSuiteResults(action_paths, config_name,
+                                                token) {
   const result_name = `test-results-${new Date().toISOString()}.html`;
   let html_results = path.join(action_paths.artifacts, config_name);
   await createTestSuiteHTMLResults(`${config_name} Test Suite Results`,
-    action_paths.artifacts, html_results);
+                                   action_paths.artifacts, html_results);
   let promise = publishArtifacts(action_paths.artifacts);
   await publishTestSuiteHTMLResults(html_results, config_name, token);
   await promise;
 }
 
-module.exports = {checkoutLibcxxIO, commitChanges, commitAndPushChanges, publishTestSuiteHTMLResults, createTestSuiteHTMLResults, createAndPublishTestSuiteResults};
+module.exports = {
+  checkoutLibcxxIO,
+  withSSHKey,
+  commitChanges,
+  commitAndPushChanges,
+  publishTestSuiteHTMLResults,
+  createTestSuiteHTMLResults,
+  createAndPublishTestSuiteResults
+};
 
 
 /***/ }),
--- publish/index.test.js
+++ publish/index.test.js
@@ -4,19 +4,24 @@ const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const {rmRf, rmRfIgnoreError, unlinkIgnoreError, run, getGitSha, unlink} = require('../src/utils');
-const {checkoutLibcxxIO, commitChanges, commitAndPushChanges, createTestSuiteHTMLResults} = require('../src/publish-action')
+const publish = require('../src/publish-action')
 const xunitViewer = require('xunit-viewer');
 
 jest.setTimeout(100000);
 
+
+
 test('checkout test', async () => {
     const out_path = path.join('/', 'tmp', 'libcxx-io');
     await rmRfIgnoreError(out_path);
-    let l = await checkoutLibcxxIO(out_path);
+    let l = await publish.withSSHKey(process.env['GITHUB_TOKEN'], async () => {
+        let r = await publish.checkoutLibcxxIO(out_path);
+        return r;
+    });
     let sha = await getGitSha(out_path);
     expect(fs.existsSync(out_path)).toBe(true);
     await run('touch', ['test.txt'], {cwd: out_path});
-    await commitChanges(out_path, 'foo bar');
+    await publish.commitChanges(out_path, 'foo bar');
     let sha2 = await getGitSha(out_path);
     expect(sha2).not.toBe(sha);
     rmRfIgnoreError(out_path);
@@ -25,16 +30,20 @@ test('checkout test', async () => {
 test('push test', async () => {
     const out_path = path.join('/', 'tmp', 'libcxx-io-2');
     await rmRfIgnoreError(out_path);
-    let l = await checkoutLibcxxIO(out_path,'ci-testing-branch');
-    let sha = await getGitSha(out_path);
-    await expect(fs.existsSync(out_path)).toBe(true);
-    const test_file = path.join(out_path, 'test.txt');
-    if (fs.existsSync(test_file)) {
-        await unlink(test_file);
-    } else {
-        await run('touch', [test_file]);
-    }
-    await commitAndPushChanges(out_path, 'pushing from test case', process.env['GITHUB_TOKEN']);
+
+    let sha = await publish.withSSHKey(process.env['GITHUB_TOKEN'], async () => {
+        await publish.checkoutLibcxxIO(out_path,'ci-testing-branch');
+        let sha = await getGitSha(out_path);
+        await expect(fs.existsSync(out_path)).toBe(true);
+        const test_file = path.join(out_path, 'test.txt');
+        if (fs.existsSync(test_file)) {
+            await unlink(test_file);
+        } else {
+            await run('touch', [test_file]);
+        }
+        let r = await publish.commitAndPushChanges(out_path, 'pushing from test case');
+        return sha;
+    });
     let sha2 = await getGitSha(out_path);
     await expect(sha2).not.toBe(sha);
     await rmRfIgnoreError(out_path);
@@ -46,7 +55,7 @@ test('build html sites', async () => {
     const output = path.join('/', 'tmp', 'results.html');
     console.log(output);
     await unlinkIgnoreError(output);
-    await expect(createTestSuiteHTMLResults("results", inputs, output)).resolves.toBe(0);
+    await expect(publish.createTestSuiteHTMLResults("results", inputs, output)).resolves.toBe(0);
     await expect(fs.existsSync(output)).toBe(true);
     await fs.unlinkSync(output);
 
